1cf6bb3de12fabea9144b28c36db296bd8b3696f
maplesyrupman <maplesyrupman@protonmail.com>
2022-01-03 23:30:39 -0500

    about & nav components

---
+++ src/components/Nav/index.js
@@ -0,0 +1,54 @@
+import React from "react";
+
+const categories = [
+    {
+        name: "commercial",
+        description:
+            "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+        name: "landscape",
+        description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+];
+
+const Nav = () => {
+    function categorySelected(category) {
+        console.log(category)
+    }
+    return (
+        <header className="flex-row">
+            <h2 className="d-inline no-wordwrap">
+                <a href='/'>
+                    <span role='img' aria-label='camera'>📸</span> Oh Snap!
+                </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className='mx-2 no-wordwrap'>
+                        <a href='#about'>
+                            About Me
+                        </a>
+                    </li>
+                    <li className="mx-2">
+                        <span>Contact</span>
+                    </li>
+                    {categories.map((category) => (
+                        <li
+                            className="mx-1"
+                            key={category.name}
+                        >
+                            <span onClick={() => categorySelected(category)}>
+                                {category.name}
+                            </span>
+                        </li>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Nav
